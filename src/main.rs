bccb7cfeaf7132931daf244ccfd4c5ad0894ec12
Alex Wellnitz <moin@wellnitz-alex.de>
2024-02-11 20:17:49 +0100

    fix: remove unused route

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-use axum::routing::post;
-use axum::{
-    routing::get,
-    Router,
-};
+use axum::{routing::get, Router};
 
 use rustysearch::search::engine::SearchEngine;
 
@@ -17,11 +13,10 @@ async fn main() {
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/", get(rustysearch::handler::hello::say_hello))
-        .route("/search/add", post(rustysearch::handler::search::index_new_document));
+        .route("/", get(rustysearch::handler::hello::say_hello));
+    // .route("/search/add", post(rustysearch::handler::search::index_new_document));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
-
