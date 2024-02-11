1891687db27bd89540486e3d8377944b1648b8a3
Alex Wellnitz <moin@wellnitz-alex.de>
2024-02-11 17:12:45 +0100

    feat: Add search test case for Rust keyword

--- tests/search_engine_tests.rs
+++ tests/search_engine_tests.rs
@@ -7,7 +7,9 @@ mod tests {
         let mut search_engine = SearchEngine::new(1.2, 0.75);
 
         search_engine.index("https://www.rust-lang.org/", "Rust Programming Language");
+        let result = search_engine.search("Rust");
 
+        assert_eq!(result.len(), 1);
         assert_eq!(search_engine.posts().len(), 1);
         assert_eq!(search_engine.number_of_documents(), 1);
     }
